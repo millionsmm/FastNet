dd98aef6955352733eb7a255ed78ad9e11153dad
millionsmm <2415122956@qq.com>
2017-08-11 18:51:35 +0800

    定义成员变量，回调接口RequestListener

--- app/src/main/java/vpour/fastnet/Request.java
+++ app/src/main/java/vpour/fastnet/Request.java
@@ -11,7 +11,32 @@ import java.util.Map;
  */
 
 public abstract class Request<T> implements Comparable<Request<T>> {
+    //默认的编码方式
+    private static final String DEFAULT_PARAMS_ENCODING = "UTF-8";
+    //请求序列号
+    protected int mSerialNum = 0;
+    //优先级默认设置为Normal
+    protected Priority mPriority = Priority.NORMAL;
+    //是否取消该请求
+    protected boolean isCancel = false;
+    //该请求是否缓存
+    private boolean mShouldCache = true;
+    //请求Listener
+    protected RequestListener<T> mRequestListener;
+    //请求的URL
+    private String mUrl = "";
+    //请求的方法
+    HttpMethod mHttpMethod = HttpMethod.GET;
+    //请求的header
+    private Map<String, String> mHeaders = new HashMap<>();
+    //请求参数
+    private Map<String, String> mBodyParams = new HashMap<>();
 
+
+    public static interface RequestListener<T> {
+        //请求完成的回调
+        public void onComplete(int stCode, T respone, String errMsg);
+    }
 
 
     //Http请求方法枚举，这里我们只有GET,POST,PUT,DELETE四种
